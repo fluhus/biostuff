25e607d6aff2dc7c517a65441c06b9eadf772318
Amit Lavon <doctor_troll@walla.co.il>
2014-05-21 12:20:56 +0300

    implemented the sam tester

--- .main/samtester.go
+++ .main/samtester.go
@@ -1,90 +1,61 @@
-/* ****************************************************************************
- * Tests a mapping performance on a SAM file.
- * ****************************************************************************/
-
+// Tests a mapping performance on a SAM file.
 package main
 
-import "fmt"
-import "flag"
-import "strconv"
-import "bioformats"
-import "strdist"
+import (
+	"os"
+	"fmt"
+	"bufio"
+	"tools"
+	"strconv"
+	"strings"
+	"bioformats/sam"
+)
 
-// Tests mapping performance on a SAM file.
-// Expects to receive the path of the target SAM, path of the reference
-// fasta and the subsequence length used when creating the reads.
-// Returns the numbers of well mapped reads, badly mapped reads and unmapped
-// reads. The 3 sum up to the total of lines in the sam.
-func testSam(samPath string, fastaPath string, ssLength int) (
-	goodMaps, badMaps, unMaps int) {
-	// Open fasta
-	fs := bioformats.NewFastaSubsequencer(bioformats.FastaFromFile(fastaPath))
-	if fs == nil {
-		panic(fmt.Sprintf("Could not open fasta: %s", fastaPath))
-	}
-	fs.SetSSLength(ssLength)
-
-	// Open SAM
-	sam := bioformats.NewSamReader(samPath)
-	if sam == nil {
-		panic(fmt.Sprintf("Could not open SAM: %s", samPath))
-	}
-
-	// Read each SAM line
-	for line := sam.NextLine(); line != nil; line = sam.NextLine() {
-		// Check if unmapped
-		if line.Pos == 0 || line.Rname == "*" {
-			unMaps++
-			continue   // Nothing to check if unmapped, move to next read
-		}
-
-		// Extract subsequence number
-		ssNumber, sserr := strconv.Atoi(line.Qname)
-		if sserr != nil {
-			panic("Bad subsequence index format: " + line.Qname)
-		}
-
-		// Extract real subsequence
-		ssReal := string(fs.GetSS(ssNumber))
-
-		// Extract mapped subsequence (-1 because it is 1 based)
-		ssMap := string(fs.GetSS2(line.Rname, line.Pos - 1))
-
-		// Compare distances; if mapped is at least as good as real, success!
-		scores := strdist.BlastDefaultScores()
-		if strdist.BlastDistance(ssMap, line.Seq, scores) <=
-			strdist.BlastDistance(ssReal, line.Seq, scores) {
-				goodMaps++
-		} else {
-			badMaps++
-		}
-	}
-
-	return
+func pe(a ...interface{}) {
+	fmt.Fprint(os.Stderr, a...)
+	fmt.Fprint(os.Stderr, "\n")
 }
 
 func main() {
-	// Parse command line arguments
-	flag.Parse()
-	args := flag.Args()
-
-	if len(args) != 3 {
-		const usage = "Tests mapping performance on a map file.\n\n" +
-			"Usage:\nsamtester <sam> <fasta ref> <subsequence length>"
-		fmt.Println(usage)
-		return
+	// Open buffer on stdin
+	in := bufio.NewReaderSize(os.Stdin, tools.Mega)
+	
+	// Read from stdin
+	pe("reading sam from stdin...")
+	var err error
+	var line *sam.Sam
+	reads := 0
+	goodMaps := 0
+	badMaps  := 0
+	unMaps   := 0
+	for line, err = sam.ReadNext(in); err == nil;
+			line, err = sam.ReadNext(in) {
+		reads++
+		
+		// Split qname to fields
+		split := strings.Split(line.Qname, ".")
+		chr := strings.Join(split[0 : len(split) - 2], "")
+		pos, _ := strconv.Atoi(split[len(split) - 2])
+		
+		// Check if mapped correctly
+		if chr == line.Rname && pos == line.Pos - 1 {
+			goodMaps++
+		} else {
+			if line.Mapq == 0 {
+				// Unmapped
+				unMaps++
+			} else {
+				// Badly mapped
+				badMaps++
+			}
+		}
 	}
-
-	// Extract subsequence length
-	ssl, errssl := strconv.Atoi(args[2])
-	if errssl != nil {
-		panic(fmt.Sprintf("Bad number format for subsequence length: %s",
-			args[2]))
-	}
-
-	// Test
-	a, b, c := testSam(args[0], args[1], ssl)
-	fmt.Printf("Good maps:\t%d\nBad maps:\t%d\nUnmaps:\t\t%d\n", a, b, c)
+	
+	pe("\nerr=", err.Error())
+	pe("\nreads\t\t", reads)
+	pe("correct\t\t", goodMaps)
+	pe("incorrect\t", badMaps)
+	pe("unmapped\t", unMaps)
 }
 
 
