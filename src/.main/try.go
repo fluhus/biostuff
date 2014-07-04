1e0c70352766f21fd2c147090bcfb418c62f0200
Amit Lavon <doctor_troll@walla.co.il>
2014-07-04 08:40:04 +0300

    changed try

--- src/.main/try.go
+++ src/.main/try.go
@@ -2,15 +2,57 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+type iterator1 struct {
+	current int
+	n int
+}
+
+func newIterator1(n int) *iterator1 {
+	return &iterator1{0, n}
+}
+
+func (it *iterator1) hasNext() bool {
+	return it.current < it.n
+}
+
+func (it *iterator1) next() int {
+	if !it.hasNext() { panic("NOOOOOOOOOOOOOOOO") }
+	it.current++
+	return it.current - 1
+}
+
+type iterator2 <-chan int
+
+func newIterator2(n int) iterator2 {
+	result := make(chan int)
+	go func() {
+		for i := 0; i < n; i++ {
+			result <- i
+		}
+		close(result)
+	}()
+	return result
+}
+
 func main() {
-	var b []byte
-	fmt.Printf("len=%d cap=%d\n", len(b), cap(b))
-	for i := 0; i < 8; i++ {
-		b = append(b, "ami"...)
-		fmt.Printf("len=%d cap=%d\n", len(b), cap(b))
-	}
+	const k = 1000000
 	
-	fmt.Println(string(b))
+	t := time.Now()
+	it1 := newIterator1(k)
+	i := 0
+	for it1.hasNext() {
+		i += it1.next()
+	}
+	fmt.Println(time.Now().Sub(t))
+	
+	t = time.Now()
+	it2 := newIterator2(k)
+	i = 0
+	for n := range it2 {
+		i += n
+	}
+	fmt.Println(time.Now().Sub(t))
 }
