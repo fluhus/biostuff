273ff0f173fc6b6e47ecdd6d6c44702cf924f678
Amit Lavon <doctor_troll@walla.co.il>
2014-09-10 22:32:59 +0300

    changed try.go

--- src/.main/try.go
+++ src/.main/try.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
-	"bioformats/fasta"
-	"time"
-	"strdist"
-	"math/rand"
+	"bioformats/fastq"
+	"bufio"
+	// "time"
+	// "strdist"
+	// "math/rand"
 	"os"
+	// "stat"
 )
 
 func pe(a ...interface{}) {
@@ -15,39 +17,52 @@ func pe(a ...interface{}) {
 
 func main() {
 	pe("start")
-	rand.Seed( time.Now().UnixNano() )
-
-	// Read fasta
-	fa, err := fasta.FastaFromFile("../aligners/fasta/Yeast.fa")
+	
+	f, err := os.Open("data\\sample.fastq")
 	if err != nil {
-		pe("error: " + err.Error())
+		pe("error opening fastq:", err.Error())
 		return
 	}
-
-	// Take only chromosome 1
-	//fa = fa[0:1]
-
-	pe("fasta length:", len(fa))
-
-	//distFunc := strdist.BigramDistance
-	const ssl = 70
-	const measurements = 10000
-
-	h := make([]int, ssl + 1)
-	e := make([]int, ssl + 1)
-
-	n := fa.NumberOfSubsequences(ssl)
-	for i := 0; i < measurements; i++ {
-		seq1,_,_ := fa.Subsequence(ssl, rand.Intn(n))
-		seq2,_,_ := fa.Subsequence(ssl, rand.Intn(n))
-
-		dists[distFunc(seq1, seq2)]++
+	
+	b := bufio.NewReader(f)
+	
+	length := -1
+	count  := 0
+	var quals [][]int
+	
+	// for fq, err := fastq.ReadNext(b); err == nil; fq, err = fastq.ReadNext(b) {
+	for i := 0; i < 100000; i++ {
+		fq, _ := fastq.ReadNext(b)
+		
+		if length == -1 {
+			length = len(fq.Quals)
+			quals = make([][]int, length)
+			pe("read length:", length)
+			
+			for j := range quals {
+				quals[j] = make([]int, 60)
+			}
+		}
+		
+		for j := range fq.Quals {
+			qual := int( fq.Quals[j] ) - 33
+			quals[j][qual]++
+		}
+		
+		count++
 	}
 	
-	for i := range dists {
-		fmt.Println(dists[i])
-	}
-
+	pe(count, "reads processed")
+	
+	// Print statistics
+	// for i := range quals {
+		// fmt.Printf("%f\t%f\t%f\t%f\n",
+				// stat.Mean(quals[i]),
+				// stat.Std(quals[i]),
+				// stat.Min(quals[i]),
+				// stat.Max(quals[i]))
+	// }
+	
 	pe("end")
 }
 
