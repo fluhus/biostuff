3076be7092d55e1f4a578416ee825f968a2a4a67
unknown <owner@yehuditb_labpc8.ekmd.huji.uni>
2014-12-03 18:12:15 +0200

    changed try.go

--- src/.main/try.go
+++ src/.main/try.go
@@ -1,27 +1,46 @@
 package main
 
 import (
+	"time"
+	"math/rand"
 	"os"
 	"fmt"
-	"tools"
-	"bioformats/fastq"
+	"bioformats/fasta"
+	"strdist"
 )
 
 func pe(a ...interface{}) {
 	fmt.Fprintln(os.Stderr, a...)
 }
 
+func pef(s string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, s, a...)
+}
+
 func main() {
 	pe("start")
-	tools.Randomize()
 	
-	f := &fastq.Fastq{}
-	f.Id = []byte("Amit")
-	f.Sequence = []byte("AAAAAAAAAA")
-	f.Quals = []byte("**********")
+	fa, err := fasta.FastaFromFile("..\\reference\\yeast.fa")
+	if err != nil {
+		pe(err.Error)
+		return
+	}
 	
-	f.ApplyQuals(fastq.Illumina18)
-	pe(f)
+	fmt.Println("loaded", len(fa), "chromosomes")
+	
+	rand.Seed(time.Now().UnixNano())
+	seq,_,_ := fa.Subsequence(50, rand.Intn(fa.NumberOfSubsequences(50)))
+	
+	distances := make([]int, 60)
+	for i := 0; i < 10000; i++ {
+		seq2,_,_ := fa.Subsequence(50, rand.Intn(fa.NumberOfSubsequences(50)))
+		d := strdist.HammingDistance(seq, seq2)
+		distances[d]++
+	}
+	
+	for i,d := range distances {
+		pef("%d:\t%d\n", i, d)
+	}
 	
 	pe("end")
 }
