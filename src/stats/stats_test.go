1721449833216da8340e0c3ecd35e705208a5796
Amit Lavon <amitlavon@cs.huji.ac.il>
2015-10-30 13:32:48 +0200

    updated stats package

---
+++ src/stats/stats_test.go
@@ -0,0 +1,23 @@
+package stats
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStats(t *testing.T) {
+	assert := assert.New(t)
+	
+	v1 := []float64 { 1, 3, 5 }
+	v2 := []float64 { 10, 7, 4 }
+	v3 := []float64 { 1, 1, 1, 1 }
+	v4 := []float64 { 1, 1, 1, 1, 0, 0 }
+	
+	assert.Equal(Sum(v1), 9.0)
+	assert.Equal(Mean(v1), 3.0)
+	assert.Equal(Var(v1), 8.0 / 3.0)
+	assert.Equal(Corr(v1, v2), -1.0)
+	assert.Equal(Ent(v3), 2.0)
+	assert.Equal(Ent(v4), 2.0)
+}
+
