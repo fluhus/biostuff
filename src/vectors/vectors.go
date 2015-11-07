db8f2d62d98b2a009edc8edc87701ff92281a176
Amit Lavon <amitlavon@cs.huji.ac.il>
2015-11-07 17:45:23 +0200

    added functions to vectors

--- src/vectors/vectors.go
+++ src/vectors/vectors.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 )
 
-// L1 (Manhattan) distance. Equivalent to Lp(1) but much faster.
+// L1 (Manhattan) distance. Equivalent to Lp(1) but more efficient.
 func L1(a, b []float64) float64 {
 	assertMatchingLengths(a, b)
 
@@ -18,7 +18,7 @@ func L1(a, b []float64) float64 {
 	return sum
 }
 
-// L2 (Euclidean) distance. Equivalent to Lp(2) but much faster.
+// L2 (Euclidean) distance. Equivalent to Lp(2) but more efficient.
 func L2(a, b []float64) float64 {
 	assertMatchingLengths(a, b)
 
@@ -72,6 +72,52 @@ func Mul(a []float64, m float64) {
 	for i := range a {
 		a[i] *= m
 	}
+}
+
+// Returns the dot product of the input vectors.
+func Dot(a, b []float64) float64 {
+	assertMatchingLengths(a, b)
+	
+	sum := 0.0
+	for i := range a {
+		sum += a[i] * b[i]
+	}
+	return sum
+}
+
+// Returns the norm of the vector, in L2.
+func Norm(a []float64) float64 {
+	norm := 0.0
+	for _, v := range a {
+		norm += v*v
+	}
+	return math.Sqrt(norm)
+}
+
+// Returns a copy of the given vector.
+func Copy(a []float64) []float64 {
+	result := make([]float64, len(a))
+	copy(result, a)
+	return result
+}
+
+// Returns the Jaccard distance between the 2 vectors. The compared sets are of
+// the non-zero elements (without comparing their actual values).
+func Jaccard(a, b []float64) float64 {
+	assertMatchingLengths(a, b)
+	
+	un := 0.0
+	in := 0.0
+	for i := range a {
+		if a[i] != 0 && b[i] != 0 {
+			un++
+			in++
+		} else if a[i] != 0 || b[i] != 0 {
+			un++
+		}
+	}
+	
+	return 1 - in / un
 }
 
 // Panics if 2 vectors are of inequal lengths.
