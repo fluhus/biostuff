e6b301f5a8052b2bef6ca469cddda2c2500c15f3
Amit Lavon <amitl@google.com>
2019-06-12 17:41:21 +0300

    removed use of assert

--- bioformats/bed/bedgraph/index_test.go
+++ bioformats/bed/bedgraph/index_test.go
@@ -1,24 +1,35 @@
 package bedgraph
 
 import (
-	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 )
 
 func TestIndex_simple(t *testing.T) {
-	assert := assert.New(t)
-	builder := NewIndexBuilder()
-	builder.Add("chr1", 0, 10, 1)
-	idx := builder.Build()
+	b := NewIndexBuilder()
+	b.Add("chr1", 0, 10, 1)
+	idx := b.Build()
 	t.Log(idx.str())
-	assert.Equal(1.0, idx.Value("chr1", 0))
-	assert.Equal(1.0, idx.Value("chr1", 9))
-	assert.Equal(0.0, idx.Value("chr1", 10))
+
+	tests := []struct {
+		chr  string
+		pos  int
+		want float64
+	}{
+		{"chr1", 0, 1.0},
+		{"chr1", 9, 1.0},
+		{"chr1", 10, 0.0},
+	}
+
+	for _, test := range tests {
+		if got := idx.Value(test.chr, test.pos); got != test.want {
+			t.Errorf("Value(%v,%v)=%v, want %v",
+				test.chr, test.pos, got, test.want)
+		}
+	}
 }
 
 func TestIndex_complex(t *testing.T) {
-	assert := assert.New(t)
-
 	builder := NewIndexBuilder()
 	builder.Add("chr1", 0, 10, 1)
 	builder.Add("chr1", 5, 15, 2)
@@ -26,36 +37,62 @@ func TestIndex_complex(t *testing.T) {
 	builder.Add("chr1", 21, 30, 8)
 	builder.Add("chr1", 25, 35, 8)
 	builder.Add("chr2", 180, 270, 60)
-
 	idx := builder.Build()
-	assert.Equal(1.0, idx.Value("chr1", 0))
-	assert.Equal(1.0, idx.Value("chr1", 4))
-	assert.Equal(3.0, idx.Value("chr1", 5))
-	assert.Equal(6.0, idx.Value("chr1", 10))
-	assert.Equal(0.0, idx.Value("chr1", 20))
-	assert.Equal(8.0, idx.Value("chr1", 21))
-	assert.Equal(16.0, idx.Value("chr1", 25))
-	assert.Equal(8.0, idx.Value("chr1", 30))
-	assert.Equal(0.0, idx.Value("chr1", 35))
 
-	assert.Equal(0.0, idx.Value("chr2", 179))
-	assert.Equal(60.0, idx.Value("chr2", 180))
-	assert.Equal(60.0, idx.Value("chr2", 200))
-	assert.Equal(60.0, idx.Value("chr2", 269))
-	assert.Equal(0.0, idx.Value("chr2", 270))
+	tests := []struct {
+		chr  string
+		pos  int
+		want float64
+	}{
+		{"chr1", 0, 1.0},
+		{"chr1", 4, 1.0},
+		{"chr1", 5, 3.0},
+		{"chr1", 10, 6.0},
+		{"chr1", 20, 0.0},
+		{"chr1", 21, 8.0},
+		{"chr1", 25, 16.0},
+		{"chr1", 30, 8.0},
+		{"chr1", 35, 0.0},
 
-	assert.Equal(0.0, idx.Value("chr3", 0))
+		{"chr2", 179, 0.0},
+		{"chr2", 180, 60.0},
+		{"chr2", 200, 60.0},
+		{"chr2", 269, 60.0},
+		{"chr2", 270, 0.0},
+
+		{"chr3", 0, 0.0},
+	}
+
+	for _, test := range tests {
+		if got := idx.Value(test.chr, test.pos); got != test.want {
+			t.Errorf("Value(%v,%v)=%v, want %v",
+				test.chr, test.pos, got, test.want)
+		}
+	}
 }
 
-func TestIndex_range(t *testing.T) {
-	assert := assert.New(t)
-
+func TestIndex_valueRange(t *testing.T) {
 	builder := NewIndexBuilder()
 	builder.Add("chr1", 5, 15, 1)
 	builder.Add("chr1", 8, 20, 2)
 	idx := builder.Build()
 
-	assert.Equal([]float64{0, 1, 1}, idx.ValueRange("chr1", 4, 7))
-	assert.Equal([]float64{0, 0, 0, 0, 0, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 0, 0, 0},
-		idx.ValueRange("chr1", 0, 23))
+	tests := []struct {
+		chr  string
+		from int
+		to   int
+		want []float64
+	}{
+		{"chr1", 4, 7, []float64{0, 1, 1}},
+		{"chr1", 0, 23, []float64{0, 0, 0, 0, 0, 1, 1, 1, 3, 3, 3, 3,
+			3, 3, 3, 2, 2, 2, 2, 2, 0, 0, 0}},
+	}
+
+	for _, test := range tests {
+		if got := idx.ValueRange(
+			test.chr, test.from, test.to); !reflect.DeepEqual(got, test.want) {
+			t.Errorf("Value(%v,%v,%v)=%v, want %v",
+				test.chr, test.from, test.to, got, test.want)
+		}
+	}
 }
--- bioformats/fasta/fasta_test.go
+++ bioformats/fasta/fasta_test.go
@@ -2,82 +2,82 @@ package fasta
 
 import (
 	"bufio"
-	"github.com/stretchr/testify/assert"
 	"strings"
 	"testing"
 )
 
 func TestReadEntry_simple(t *testing.T) {
-	assert := assert.New(t)
-
 	input := ">foo\nAaTtGnNngcCaN"
-
 	fa, err := ReadEntry(bufio.NewReader(strings.NewReader(input)))
-	assert.Nil(err, "Error reading fasta: %v", err)
-
-	assertFasta(assert, fa, "foo", "AaTtGnNngcCaN")
-	assert.Equal(2, len(fa.nEnds))
+	if err != nil {
+		t.Fatalf("ReadEntry(%q) failed: %v", input, err)
+	}
+	checkFasta(t, fa, "foo", "AaTtGnNngcCaN")
+	//assert.Equal(2, len(fa.nEnds))
 }
 
 func TestReadEntry_noName(t *testing.T) {
-	assert := assert.New(t)
-
 	input := "AaTtGngcCaN"
-
 	fa, err := ReadEntry(bufio.NewReader(strings.NewReader(input)))
-	assert.Nil(err, "Error reading fasta: %v", err)
-
-	assertFasta(assert, fa, "", input)
+	if err != nil {
+		t.Fatalf("ReadEntry(%q) failed: %v", input, err)
+	}
+	checkFasta(t, fa, "", input)
 }
 
 func TestReadEntry_multiline(t *testing.T) {
-	assert := assert.New(t)
-
 	input := ">foo\nAaTtGngcCaN\nGGgg\n>foo"
 
 	fa, err := ReadEntry(bufio.NewReader(strings.NewReader(input)))
-	assert.Nil(err, "Error reading fasta: %v", err)
-
-	assertFasta(assert, fa, "foo", "AaTtGngcCaNGGgg")
+	if err != nil {
+		t.Fatalf("ReadEntry(%q) failed: %v", input, err)
+	}
+	checkFasta(t, fa, "foo", "AaTtGngcCaNGGgg")
 }
 
 func TestReadEntry_badChars(t *testing.T) {
-	assert := assert.New(t)
-
 	input := ">foo\naaaaaaKaaaaa"
-
 	_, err := ReadEntry(bufio.NewReader(strings.NewReader(input)))
-	assert.NotNil(err, "Expected error for non-nucleotide character.")
+	if err == nil {
+		t.Fatalf("ReadEntry(%q) succeeded, want fail", input)
+	}
 }
 
 func TestReadFasta_simple(t *testing.T) {
-	assert := assert.New(t)
-
 	input := ">foo\nAaTtGngcCaN\nGGgg\n>bar\naaaGcgnnNcTAtgGa\nAA\n\nGagaGNtCc"
 
 	fas, err := ReadFasta(strings.NewReader(input))
-	assert.Nil(err, "Error reading fasta: %v", err)
-
-	assert.Equal(2, len(fas), "Bad number of entries read.")
-	assertFasta(assert, fas[0], "foo", "AaTtGngcCaNGGgg")
-	assertFasta(assert, fas[1], "bar", "aaaGcgnnNcTAtgGaAAGagaGNtCc")
+	if err != nil {
+		t.Fatalf("ReadFasta(%q) failed: %v", input, err)
+	}
+	if len(fas) != 2 {
+		t.Fatalf("len(ReadFasta(%q))=%v, want 2", input, len(fas))
+	}
+	checkFasta(t, fas[0], "foo", "AaTtGngcCaNGGgg")
+	checkFasta(t, fas[1], "bar", "aaaGcgnnNcTAtgGaAAGagaGNtCc")
 }
 
-func assertFasta(assert *assert.Assertions, fa *Entry, name string,
-	sequence string) {
-	assert.Equal(fa.Name(), name, "Bad name.")
-
-	assert.Equal(len(sequence), fa.Length(), "Bad fasta length.")
-
-	usequence := strings.ToUpper(sequence)
-	for i := range usequence {
-		assert.Equal(usequence[i], fa.At(i), "Bad character at index %d.", i)
+func checkFasta(t *testing.T, fa *Entry, name string, sequence string) {
+	if name != fa.Name() {
+		t.Fatalf("Name()=%q, want %q", fa.Name(), name)
 	}
 
+	usequence := strings.ToUpper(sequence)
+	fasequence := ""
+	for i := 0; i < fa.Length(); i++ {
+		fasequence += string([]byte{fa.At(i)})
+	}
+	if usequence != fasequence {
+		t.Fatalf("At(...)=%q, want %q", fasequence, usequence)
+	}
 	for i := range usequence {
 		for l := 0; l < len(usequence)-i; l++ {
-			assert.Equal(usequence[i:i+l], string(fa.Subsequence(i, l)),
-				"Bad subsequence starting at %d length %d.", i, l)
+			usub := usequence[i : i+l]
+			fasub := string(fa.Subsequence(i, l))
+			if usub != fasub {
+				t.Fatalf("Subsequence(%v,%v)=%q, want %q",
+					i, l, fasub, usub)
+			}
 		}
 	}
 }
