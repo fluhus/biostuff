867d406026af7e857d8b327587bd4516a8b9e684
Amit Lavon <amit.lavon@weizmann.ac.il>
2021-02-20 20:08:46 +0200

    Remove archive and old scripts

--- .archive/makemain.r.bat
+++
@@ -1,3 +0,0 @@
-if [%1]==[p] echo MAIN: try.go && goto :end
-move try.go try.goo >nul && echo REVERTED: try.go 
-:end
--- .scripts/celegans_fa_fix.awk
+++
@@ -1,7 +0,0 @@
-{
-	if (substr($1, 1, 1) == ">") {
-		print $1;
-	} else {
-		print;
-	}
-}
