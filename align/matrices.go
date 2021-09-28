91be656fd0a2a073e683a70e6926dff3564e7679
fluhus <doctor_troll@walla.co.il>
2021-09-28 18:32:48 +0300

    Add PAM matrices to align

--- align/matrices.go
+++ align/matrices.go
@@ -2,4 +2,9 @@ package align
 
 var (
 	Levenshtein SubstitutionMatrix // Returns negated edit distance.
+	PAM100      SubstitutionMatrix
+	PAM120      SubstitutionMatrix
+	PAM160      SubstitutionMatrix
+	PAM200      SubstitutionMatrix
+	PAM250      SubstitutionMatrix
 )
