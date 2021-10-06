4a0f9f3ff639cd398688676a58ef9bd581403cc3
fluhus <doctor_troll@walla.co.il>
2021-10-06 22:42:52 +0300

    Add BLOSUM and clean up PAM in align

--- align/matrices.go
+++ align/matrices.go
@@ -2,9 +2,10 @@ package align
 
 var (
 	Levenshtein SubstitutionMatrix // Returns negated edit distance.
-	PAM100      SubstitutionMatrix
 	PAM120      SubstitutionMatrix
 	PAM160      SubstitutionMatrix
-	PAM200      SubstitutionMatrix
 	PAM250      SubstitutionMatrix
+	BLOSUM80    SubstitutionMatrix
+	BLOSUM62    SubstitutionMatrix
+	BLOSUM45    SubstitutionMatrix
 )
