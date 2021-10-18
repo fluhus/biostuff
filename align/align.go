94c09fb217373ea0a011fe368f76c9b0040fb214
fluhus <doctor_troll@walla.co.il>
2021-10-18 18:49:31 +0300

    Implement local alignment

--- align/align.go
+++ align/align.go
@@ -90,11 +90,11 @@ type block struct {
 }
 
 // Global performs global alignment on a and b and finds the highest scoring
-// alignment. Returns the steps where each step is on a single character, relating
-// to a. Time and space complexities are O(len(a)*len(b)).
+// alignment. Returns the steps relating to a, and the alignment score.
+// Time and space complexities are O(len(a)*len(b)).
 //
 // Uses the Needleman-Wunsch algorithm.
-func Global(a, b []byte, m SubstitutionMatrix) ([]Step, float64) {
+func Global(a, b []byte, m SubstitutionMatrix) (steps []Step, score float64) {
 	an, bn := len(a)+1, len(b)+1
 	blocks := make([]block, an*bn)
 	for i := range blocks {
@@ -137,6 +137,66 @@ func Global(a, b []byte, m SubstitutionMatrix) ([]Step, float64) {
 	return traceAlignmentSteps(blocks, bn)
 }
 
+// Local performs local alignment on a and b and finds the highest scoring
+// alignment. Returns the steps relating to a, ai and bi as the start positions of
+// the local alignment in a and b respectively, and the alignment score.
+// Time and space complexities are O(len(a)*len(b)).
+//
+// Uses the Smith-Waterman algorithm.
+func Local(a, b []byte, m SubstitutionMatrix) (
+	steps []Step, ai, bi int, score float64) {
+	an, bn := len(a)+1, len(b)+1
+	blocks := make([]block, an*bn)
+	for i := range blocks {
+		ai, bi := i/bn, i%bn
+
+		// Edges of the matrix.
+		if ai == 0 && bi == 0 {
+			continue
+		}
+		if ai == 0 {
+			blocks[i].step = Insertion
+			blocks[i].score = blocks[i-1].score + m.Get(Gap, b[bi-1])
+			if bi == 1 { // New gap
+				blocks[i].score += m.Get(Gap, Gap)
+			}
+			if blocks[i].score < 0 {
+				blocks[i] = block{0, 0}
+			}
+			continue
+		}
+		if bi == 0 {
+			blocks[i].step = Deletion
+			blocks[i].score = blocks[i-bn].score + m.Get(a[ai-1], Gap)
+			if ai == 1 { // New gap
+				blocks[i].score += m.Get(Gap, Gap)
+			}
+			if blocks[i].score < 0 {
+				blocks[i] = block{0, 0}
+			}
+			continue
+		}
+
+		// Middle of matrix. Calculate the score of each possible step.
+		mch := blocks[i-bn-1].score + m.Get(a[ai-1], b[bi-1])
+		del := blocks[i-bn].score + m.Get(a[ai-1], Gap)
+		if blocks[i-bn].step != Deletion {
+			del += m.Get(Gap, Gap)
+		}
+		ins := blocks[i-1].score + m.Get(Gap, b[bi-1])
+		if blocks[i-1].step != Insertion {
+			ins += m.Get(Gap, Gap)
+		}
+		blocks[i] = decideOnStep(mch, del, ins)
+		if blocks[i].score < 0 {
+			blocks[i] = block{0, 0}
+		}
+	}
+
+	steps, i, score := traceAlignmentStepsLocal(blocks, bn)
+	return steps, i/bn - 1, i%bn - 1, score
+}
+
 // Returns a block with the highest scoring step.
 func decideOnStep(mch, del, ins float64) block {
 	if mch >= del && mch >= ins {
@@ -172,6 +232,56 @@ func traceAlignmentSteps(blocks []block, bn int) ([]Step, float64) {
 		steps[i], steps[len(steps)-1-i] = steps[len(steps)-1-i], steps[i]
 	}
 	return steps, blocks[len(blocks)-1].score
+}
+
+// Reproduces the alignment steps that lead to the final highest score.
+// Returns the steps and their score.
+func traceAlignmentStepsLocal(blocks []block, bn int) ([]Step, int, float64) {
+	imax := argmax(blocks)
+	steps := make([]Step, 0, bn+len(blocks)/bn)
+	i := imax
+	last := i
+	for i > 0 {
+		if blocks[i].score < 0 {
+			panic(fmt.Sprintf("bad score at i=%d: %f", i, blocks[i].score))
+		}
+		if blocks[i].score == 0 {
+			break
+		}
+		last = i
+		steps = append(steps, blocks[i].step)
+		switch blocks[i].step {
+		case Match:
+			i -= bn + 1
+		case Deletion:
+			i -= bn
+		case Insertion:
+			i -= 1
+		}
+	}
+	if i < 0 {
+		panic(fmt.Sprintf("bad i: %v, expected 0", i))
+	}
+	// Check placed here and not in the begining to go allow checks.
+	if blocks[imax].score == 0 {
+		return nil, 0, 0
+	}
+	// Reverse steps.
+	for i := 0; i < len(steps)/2; i++ {
+		steps[i], steps[len(steps)-1-i] = steps[len(steps)-1-i], steps[i]
+	}
+	return steps, last, blocks[imax].score
+}
+
+// Returns the index of the highest score block.
+func argmax(blocks []block) int {
+	imax := 0
+	for i, b := range blocks {
+		if b.score > blocks[imax].score {
+			imax = i
+		}
+	}
+	return imax
 }
 
 // GoString implements the fmt.GoStringer interface.
--- align/align_test.go
+++ align/align_test.go
@@ -62,21 +62,64 @@ func TestGlobal(t *testing.T) {
 		{Gap, Gap}: -1,
 	}.Symmetrical()
 	tests := []struct {
-		a, b      []byte
+		a, b      string
 		want      []Step
 		wantScore float64
 	}{
-		{[]byte("aba"), []byte("aba"), []Step{Match, Match, Match}, 3},
-		{[]byte("aba"), []byte("aa"), []Step{Match, Deletion, Match}, 1},
-		{[]byte("aa"), []byte("aba"), []Step{Match, Insertion, Match}, 1},
+		{"aba", "aba", []Step{Match, Match, Match}, 3},
+		{"aba", "aa", []Step{Match, Deletion, Match}, 1},
+		{"aa", "aba", []Step{Match, Insertion, Match}, 1},
 	}
 	for _, test := range tests {
-		got, gotScore := Global(test.a, test.b, m)
+		got, gotScore := Global([]byte(test.a), []byte(test.b), m)
 		if !reflect.DeepEqual(got, test.want) {
 			t.Errorf("Global(%q,%q)=%v, want %v", test.a, test.b, got, test.want)
 		}
 		if gotScore != test.wantScore {
-			t.Errorf("Global(%q,%q)= score=%v, want %v",
+			t.Errorf("Global(%q,%q) score=%v, want %v",
+				test.a, test.b, gotScore, test.wantScore)
+		}
+	}
+}
+
+func TestLocal(t *testing.T) {
+	m := SubstitutionMatrix{
+		{'a', 'a'}: 1,
+		{'b', 'b'}: 1,
+		{'c', 'c'}: 1,
+		{'a', 'b'}: -1,
+		{'a', 'c'}: -1,
+		{'b', 'c'}: -1,
+		{'a', Gap}: -1,
+		{'b', Gap}: -1,
+		{'c', Gap}: -1,
+		{Gap, Gap}: 0,
+	}.Symmetrical()
+	tests := []struct {
+		a, b           string
+		want           []Step
+		wantAI, wantBI int
+		wantScore      float64
+	}{
+		{"aaaa", "aaaa", []Step{Match, Match, Match, Match}, 0, 0, 4},
+		{"aaaa", "bbbb", nil, -1, -1, 0},
+		{"bbaaaacc", "ccaaaabb", []Step{Match, Match, Match, Match}, 2, 2, 4},
+	}
+	for _, test := range tests {
+		got, ai, bi, gotScore := Local([]byte(test.a), []byte(test.b), m)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("Local(%q,%q)=%v, want %v", test.a, test.b, got, test.want)
+		}
+		if ai != test.wantAI {
+			t.Errorf("Local(%q,%q) ai=%v, want %v",
+				test.a, test.b, ai, test.wantAI)
+		}
+		if bi != test.wantBI {
+			t.Errorf("Local(%q,%q) bi=%v, want %v",
+				test.a, test.b, bi, test.wantBI)
+		}
+		if gotScore != test.wantScore {
+			t.Errorf("Local(%q,%q) score=%v, want %v",
 				test.a, test.b, gotScore, test.wantScore)
 		}
 	}
