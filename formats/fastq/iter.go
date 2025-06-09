5a7d493da9c4e1f642761a32b56238d01438d8d9
Amit Lavon <amitlavon1.spam@gmail.com>
2025-06-09 10:58:36 -0700

    Add FilePaired to fastq

--- formats/fastq/iter.go
+++ formats/fastq/iter.go
@@ -1,6 +1,7 @@
 package fastq
 
 import (
+	"fmt"
 	"io"
 	"iter"
 
@@ -48,6 +49,45 @@ func Reader(r io.Reader) iter.Seq2[*Fastq, error] {
 		for fa, err := range newReader(r).iter() {
 			if !yield(fa, err) {
 				break
+			}
+		}
+	}
+}
+
+// FilePaired returns an iterator over fastq entries in two paired files.
+// Yields pairs of entries, one from each file.
+// The files are expected to have the same number of reads.
+func FilePaired(file1, file2 string) iter.Seq2[[]*Fastq, error] {
+	return func(yield func([]*Fastq, error) bool) {
+		next1, stop1 := iter.Pull2(File(file1))
+		next2, stop2 := iter.Pull2(File(file2))
+		defer stop1()
+		defer stop2()
+
+		for {
+			fq1, err1, ok1 := next1()
+			fq2, err2, ok2 := next2()
+			if ok1 != ok2 { // One ran out before the other.
+				if ok1 {
+					yield(nil, fmt.Errorf("input 2 ran out before input 1"))
+				} else {
+					yield(nil, fmt.Errorf("input 1 ran out before input 2"))
+				}
+				return
+			}
+			if !ok1 { // All done.
+				return
+			}
+			if err1 != nil {
+				yield(nil, fmt.Errorf("input 1: %w", err1))
+				return
+			}
+			if err2 != nil {
+				yield(nil, fmt.Errorf("input 2: %w", err2))
+				return
+			}
+			if !yield([]*Fastq{fq1, fq2}, nil) {
+				return
 			}
 		}
 	}
